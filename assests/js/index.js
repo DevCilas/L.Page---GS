b8d7f3bb1fb3250f6ed2bcdbb9c01646f5c796e3
DevCilas <160146962+DevCilas@users.noreply.github.com>
2024-11-22 10:58:09 -0300

    Feat - Login Created

--- assests/js/index.js
+++ assests/js/index.js
@@ -1,7 +1,8 @@
 var login_form = document.querySelector('.form');
 
 
-document.querySelector('#botao').addEventListener('click', function () {
+
+document.querySelector('#botao1').addEventListener('click', function () {
     if (login_form.style.display === 'none' || login_form.style.display === '') {
         login_form.style.display = 'block';
     } else {
@@ -9,7 +10,7 @@ document.querySelector('#botao').addEventListener('click', function () {
     }
 })
 
-document.querySelector('#entrar').addEventListener('click', function () {
+document.querySelector('#botao2').addEventListener('click', function () {
     if (login_form.style.display === 'none' || login_form.style.display === '') {
         login_form.style.display = 'block';
     } else {
@@ -17,7 +18,81 @@ document.querySelector('#entrar').addEventListener('click', function () {
     }
 })
 
-function submitLogin() {
-    alert ('Login realizado!');
+document.querySelector('#botao3').addEventListener('click', function () {
+    if (login_form.style.display === 'none' || login_form.style.display === '') {
+        login_form.style.display = 'block';
+    } else {
         login_form.style.display = 'none';
+    }
+})
+
+document.querySelector('#botao4').addEventListener('click', function () {
+    if (login_form.style.display === 'none' || login_form.style.display === '') {
+        login_form.style.display = 'block';
+    } else {
+        login_form.style.display = 'none';
+    }
+})
+
+document.querySelector('#botao5').addEventListener('click', function () {
+    if (login_form.style.display === 'none' || login_form.style.display === '') {
+        login_form.style.display = 'block';
+    } else {
+        login_form.style.display = 'none';
+    }
+})
+
+document.querySelector('#botao6').addEventListener('click', function () {
+    if (login_form.style.display === 'none' || login_form.style.display === '') {
+        login_form.style.display = 'block';
+    } else {
+        login_form.style.display = 'none';
+    }
+})
+
+document.querySelector('#botao7').addEventListener('click', function () {
+    if (login_form.style.display === 'none' || login_form.style.display === '') {
+        login_form.style.display = 'block';
+    } else {
+        login_form.style.display = 'none';
+    }
+})
+document.querySelector('#botao8').addEventListener('click', function () {
+    if (login_form.style.display === 'none' || login_form.style.display === '') {
+        login_form.style.display = 'block';
+    } else {
+        login_form.style.display = 'none';
+    }
+})
+
+
+
+
+function logar() {
+    var user = document.getElementById('usuario').value;
+    var senha = document.getElementById('senha').value;
+    if(user == "admin" && senha == "admin"){
+        Swal.fire({
+            title: 'Login efetuado com sucesso!',
+            text: 'Você será redirecionado',
+            icon: 'success',
+            confirmButtonText: 'OK'
+            
+            
+          })
+        login_form.style.display = 'none';
+        
+        
+    } else{
+        Swal.fire(
+            {
+            title: 'Usuário ou senha incorretos',
+            text: 'Tente novamente',
+            icon: 'error'
+            
+            
+          }
+        )
+    }
+    
 }

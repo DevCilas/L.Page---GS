5e8aaac873974e1958e9f347ab1e64ad329302df
DevCilas <160146962+DevCilas@users.noreply.github.com>
2024-11-22 21:56:14 -0300

    Feat - Cadastro

--- assests/js/index.js
+++ assests/js/index.js
@@ -58,13 +58,7 @@ document.querySelector('#botao7').addEventListener('click', function () {
         login_form.style.display = 'none';
     }
 })
-document.querySelector('#botao8').addEventListener('click', function () {
-    if (login_form.style.display === 'none' || login_form.style.display === '') {
-        login_form.style.display = 'block';
-    } else {
-        login_form.style.display = 'none';
-    }
-})
+
 
 
 
@@ -114,3 +108,64 @@ function logar() {
     }
     
 }
+
+
+// Formulário de cadastro
+
+var register_form = document.querySelector('.registro');
+
+document.querySelector('#botao8').addEventListener('click', function () {
+    if (register_form.style.display === 'none' || register_form.style.display === '') {
+        register_form.style.display = 'block';
+    } else {
+        register_form.style.display = 'none';
+    }
+})
+
+
+function cadastrar() {
+    var user = document.getElementById('usuario_cadastro').value;
+    var senha = document.getElementById('senha_cadastro').value;
+    if(user !== '' && senha !== ''){
+        Swal.fire({
+            title: 'Cadastro efetuado com sucesso!',
+            text: `Usuário: ${user}    ----   Senha: ${senha}`,
+            icon: 'success',
+            confirmButtonText: 'OK',
+            confirmButtonColor: '#00b73f',
+           
+            
+            
+          }).then((result) => {
+           window.location.href = "assests/pages/home.html";
+
+        })
+        login_form.style.display = 'none';
+        
+        
+    } else{
+        Swal.fire(
+            {
+            title: 'Certifique-se de preencher todos os campos',
+            text: 'Tente novamente',
+            icon: 'error',
+            confirmButtonText: 'OK',
+            showCancelButton: 'Cancelar',
+            cancelButtonText: 'Cancelar',
+            confirmButtonColor: '#00b73f',
+            cancelButtonColor: '#ea4335',
+            
+       
+            
+            
+          }
+        ).then((result) => {
+            if (result.isDismissed) {
+                location.reload()
+            } 
+
+        })
+    }
+    
+}
+
